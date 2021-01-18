8985a1483d337af8dc29741cf6cbb9b2e65017f5
hebergb <hebergb98@gmail.com>
2021-01-18 09:03:38 -0600

    feat(server): setup config, health, db modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from './config/config.module';
+import { TerminusModule } from '@nestjs/terminus';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './config/mongoose-config.service';
+import { HealthController } from './health.controller';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    ConfigModule,
+    TerminusModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: MongooseConfigService,
+    }),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+    }),
+  ],
+  controllers: [HealthController],
   providers: [],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config: ConfigService = app.get(ConfigService.name);
+  await app.listen(config.env.APP_PORT);
 }
 bootstrap();
