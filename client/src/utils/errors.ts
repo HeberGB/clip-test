b525ee0ba7697ac0b21546576e18af9e7ae7514f
hebergb <hebergb98@gmail.com>
2021-01-19 16:29:33 -0600

    fix(client):  add default message error

--- client/src/utils/errors.ts
+++ client/src/utils/errors.ts
@@ -1,7 +1,8 @@
 import { DUPLICATE_ERR } from "../constants/errors";
 
-export function getMessageError(rawMessage: string) {
+export function getMessageError(rawMessage: string): string {
   if (rawMessage.includes(DUPLICATE_ERR)) {
     return "Your email already exists";
   }
+  return `Check technical support with the following error:: ${rawMessage}`;
 }
